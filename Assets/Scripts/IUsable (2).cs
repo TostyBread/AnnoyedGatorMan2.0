02edd0004fd68dd489a7904804423fd74a621785
YeongCheeSeng <yeongcheeseng95@gmail.com>
2025-04-27 00:48:06 +0800

    merge test

--- Assets/Scripts/IUsable (2).cs
+++
@@ -1,6 +0,0 @@
-public interface IUsable
-{
-    void EnableUsableFunction(); // Enable the usable function
-    void DisableUsableFunction(); // Disable the usable function
-    void Use(); // General method for using the item (e.g., Fire, Stab)
-}
