557415ea1a2a68c0b48a7d7ef8871e22ebe6e475
edgees <yeongcheeseng7@gmail.com>
2025-07-20 02:21:01 +0800

    follow along dialogue tutorial

---
+++ Assets/Scripts/Keat/Dialog/DialogueManager.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+//Tutorial: https://www.youtube.com/watch?v=DOP_G5bsySA&t=6s
+public class DialogueManager : MonoBehaviour
+{
+    public static DialogueManager Instance;
+
+    public Image characterIcon;
+    public TextMeshProUGUI characterName;
+    public TextMeshProUGUI dialogueArea;
+
+    private Queue<DialogueLine> lines;
+
+    public bool isDialogueActive = false;
+
+    public float typingSpeed = 0.2f;
+
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Instance == null)
+            Instance = this;
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        isDialogueActive = true;
+
+        animator.Play("show");
+        
+        lines.Clear();
+
+        foreach (DialogueLine dialogueLine in dialogue.dialogueLines) 
+        { 
+            lines.Enqueue(dialogueLine);
+        }
+
+        DisplayNextDialogueLine();
+    }
+
+    public void DisplayNextDialogueLine()
+    {
+        if (lines.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        DialogueLine currentLine = lines.Dequeue();
+
+        characterIcon.sprite = currentLine.character.icon;
+        characterName.text = currentLine.character.name; 
+
+        StopAllCoroutines();
+
+        StartCoroutine(TypeSentence(currentLine));
+
+    }
+
+    private IEnumerator TypeSentence(DialogueLine dialogueLine)
+    {
+        dialogueArea.text = "";
+        foreach (char letter in dialogueLine.line.ToCharArray())
+        { 
+            dialogueArea.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    private void EndDialogue() 
+    {
+        isDialogueActive = false;
+        animator.Play("hide");
+    }
+
+}
