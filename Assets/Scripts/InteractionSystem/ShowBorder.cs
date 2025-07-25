7267f3ee17807ecf3e290d0cebbf7e1761cccb93
YeongCheeSeng <yeongcheeseng95@gmail.com>
2025-07-25 19:01:03 +0800

    map updated

--- Assets/Scripts/InteractionSystem/ShowBorder.cs
+++ Assets/Scripts/InteractionSystem/ShowBorder.cs
@@ -7,11 +7,13 @@ public class ShowBorder : MonoBehaviour
     // This script needs to work with DetectTarget script
     public Sprite spriteWithBorder;
     public float spriteWithBorderScale = 1; // Scale factor for the sprite with border
-    private Vector3 spriteScale;
+
+    private ItemSystem itemSystem; // Reference to ItemSystem if needed
 
     private SpriteRenderer spriteRenderer;
     private Sprite originalSprite;
     private Vector3 originalTransform;
+    private Vector3 spriteScale;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +22,7 @@ public class ShowBorder : MonoBehaviour
         originalSprite = spriteRenderer.sprite;
         originalTransform = transform.localScale;
         spriteScale = new Vector3(spriteWithBorderScale, spriteWithBorderScale, spriteWithBorderScale); // Set the scale to match the spriteWithBorderScale
+        itemSystem = transform.parent.GetComponent<ItemSystem>(); // Get the ItemSystem component if it exists
     }
 
     public void ShowBorderSprite()
