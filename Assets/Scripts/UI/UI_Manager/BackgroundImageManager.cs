8bcc545532d4d6b3ce3f25c2019c314600f6e67a
YeongCheeSeng <yeongcheeseng95@gmail.com>
2025-08-07 15:11:29 +0800

    falling gator shadow updated

--- Assets/Scripts/UI/UI_Manager/BackgroundImageManager.cs
+++ Assets/Scripts/UI/UI_Manager/BackgroundImageManager.cs
@@ -6,21 +6,27 @@ using UnityEngine.UI;
 public class BackgroundImageManager : MonoBehaviour
 {
     public Animator FallingGator;
+    public Animator FallingGatorShadow;
 
     // Start is called before the first frame update
     void Start()
     {
         FallingGator.gameObject.SetActive(false);
+        FallingGatorShadow.gameObject.SetActive(false);
     }
 
     public void ShowFallingGator()
     {
         FallingGator.gameObject.SetActive(true);
         FallingGator.Play("FallingGatorAnimation");
+
+        FallingGatorShadow.gameObject.SetActive(true);
+        FallingGatorShadow.Play("ShadowAnimation");
     }
 
     public void HideFallingGator()
     {
         FallingGator.gameObject.SetActive(false);
+        FallingGatorShadow.gameObject.SetActive(false);
     }
 }
