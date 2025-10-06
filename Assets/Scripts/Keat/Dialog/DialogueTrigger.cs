39df84ab687a17992d69c265ae1df7bedad41328
YeongCheeSeng <yeongcheeseng95@gmail.com>
2025-10-06 23:46:57 +0800

    dialogue tutorial updated (WIP)

--- Assets/Scripts/Keat/Dialog/DialogueTrigger.cs
+++ Assets/Scripts/Keat/Dialog/DialogueTrigger.cs
@@ -1,8 +1,7 @@
 using System.Collections.Generic;
-using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
-//Tutorial: https://www.youtube.com/watch?v=DOP_G5bsySA&t=6s
 
+#region Serializable Data Classes
 [System.Serializable]
 public class DialogueCharacter
 {
@@ -14,8 +13,7 @@ public class DialogueCharacter
 public class DialogueLine
 {
     public DialogueCharacter character;
-    [TextArea(3, 10)]
-    public string line;
+    [TextArea(3, 10)] public string line;
 }
 
 [System.Serializable]
@@ -23,19 +21,102 @@ public class Dialogue
 {
     public List<DialogueLine> dialogueLines = new List<DialogueLine>();
 }
+#endregion
 
 public class DialogueTrigger : MonoBehaviour
 {
+    [Header("Dialogue Data")]
     public Dialogue dialogue;
 
-    [Header("Next Dialogue Reference")]
+    [Header("Next Dialogue Trigger")]
     public DialogueManager dialogueManager;
     public GameObject nextDialogue;
+    public GameObject showNextGameObject;
+
+    [Header("Trigger Condition")]
+    public DialogueConditionType triggerCondition = DialogueConditionType.None;
+    public GameObject playerGetToTriggerNext;
+
+    public enum DialogueConditionType { None, OnStove, OnItemPickup }
+
+    private CookingStove stove;
+    private GameObject player;
+    private GameObject wieldingHand;
+    private bool hasTriggered = false;
+    private bool nextTriggered = false;
 
     private void OnEnable()
     {
         dialogueManager = FindObjectOfType<DialogueManager>();
-        if (nextDialogue != null) nextDialogue.SetActive(false);
+
+        if (nextDialogue != null)
+            nextDialogue.SetActive(false);
+
+        if (triggerCondition == DialogueConditionType.OnStove)
+            stove = FindAnyObjectByType<CookingStove>();
+
+        if (triggerCondition == DialogueConditionType.OnItemPickup)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+            wieldingHand = player.transform.Find("HandControls/Wielding_Hand")?.gameObject;
+        }
+    }
+
+    private void Update()
+    {
+        switch (triggerCondition)
+        {
+            case DialogueConditionType.OnStove:
+                CheckStoveCondition();
+                break;
+
+            case DialogueConditionType.OnItemPickup:
+                CheckItemPickupCondition();
+                break;
+        }
+
+        Debug.Log("Player: " + player);
+        Debug.Log("Wielding : " + wieldingHand);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!hasTriggered && collision.CompareTag("Player"))
+        {
+            hasTriggered = true;
+            TriggerDialogue();
+
+            if (triggerCondition == DialogueConditionType.None)
+                TriggerNextDialogue();
+        }
+    }
+
+    private void CheckStoveCondition()
+    {
+        if (stove != null && stove.isOn && !nextTriggered)
+        {
+            nextTriggered = true;
+            TriggerNextDialogue();
+            stove = null;
+        }
+    }
+
+    private void CheckItemPickupCondition()
+    {
+        if (nextTriggered || player == null || playerGetToTriggerNext == null) return;
+
+        if (wieldingHand == null) return;
+
+        foreach (Transform child in wieldingHand.transform)
+        {
+            if (child.name.Contains(playerGetToTriggerNext.name))
+            {
+                nextTriggered = true;
+                TriggerNextDialogue();
+                playerGetToTriggerNext = null;
+                break;
+            }
+        }
     }
 
     public void TriggerDialogue()
@@ -46,17 +127,14 @@ public class DialogueTrigger : MonoBehaviour
             Debug.LogWarning("DialogueManager.Instance is null. Did you forget to place it in the scene?");
     }
 
-    private bool hasTriggered = false;
-
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void TriggerNextDialogue()
     {
-        if (!hasTriggered && collision.CompareTag("Player"))
-        {
-            TriggerDialogue();
-            hasTriggered = true; // prevent repeated triggering
+        if (nextDialogue != null)
+            nextDialogue.SetActive(true);
 
-            if (nextDialogue != null) nextDialogue.SetActive(true);
-            gameObject.SetActive(false);
-        }
+        if (showNextGameObject != null)
+            showNextGameObject.SetActive(true);
+
+        gameObject.SetActive(false);
     }
 }
